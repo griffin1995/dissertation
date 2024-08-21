6e70a15abc240fc12cb548073d4a806b4228cbce
griffin1995 <jtgriffin95@gmail.com>
2024-08-21 22:19:41 +0100

    front end template

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+// Import React to enable JSX syntax and use React features
+import react from 'react';
+
+function App() {
+  return (
+    <>
+      {/* Main component of the application. Currently, it returns an empty fragment */}
+    </>
+  );
+}
+
+// Export the App component as the default export of this module
+export default App;
---
+++ frontend/src/main.jsx
@@ -0,0 +1,15 @@
+// Import StrictMode from React to highlight potential problems in an application
+import { StrictMode } from "react";
+
+// Import createRoot from React DOM to create a root DOM node for the React app
+import { createRoot } from "react-dom/client";
+
+// Import the main App component from the local App.jsx file
+import App from "./App.jsx";
+
+// Create a root element at the 'root' DOM node and render the App component inside StrictMode
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
